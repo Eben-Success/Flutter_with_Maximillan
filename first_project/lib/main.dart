0244c79783fbfe84a09477486a58b525b79b9244
Eben-Success <aebenezer237@gmail.com>
2022-06-19 07:35:47 -0700

    null buttons added

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -5,9 +5,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
+  }
   Widget build(BuildContext context) {
+    var question = ['What\'s your name?', 
+    'What\'s your favorite color?'];
     return MaterialApp(
-      home: Text('Hello!'),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('What is your favourite color?'),
+        ),
+        body: Column(children: [
+          Text('The question!'),
+          RaisedButton(child: Text('Answer 1'), onPressed: null,),
+           RaisedButton(child: Text('Answer 1'), onPressed: null,),
+            RaisedButton(child: Text('Answer 1'), onPressed: null,),
+        ],),
+        ),
+      // ignore: dead_code
+      );
   }
 }
